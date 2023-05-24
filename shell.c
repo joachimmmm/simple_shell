ff348a48704eedead61872be0e01ad5a55869ca5
Joachimm <joachimokoh23@gmail.com>
2023-05-24 16:51:33 +0100

    test

--- shell.c
+++ shell.c
@@ -47,31 +47,38 @@ int check_command_existence(const char *command, char *full_path)
 	char *dir;
 	char *path_copy = _strdup(path);
 	size_t command_len = _strlen(command);
-	
-	if (path_copy == NULL)
-	{
-		return (0);
-	}
-	dir = strtok(path_copy, ":");
-	while (dir != NULL)
-	{
-		size_t dir_len = _strlen(dir);
-		size_t full_path_len = dir_len + 1 + command_len;
 
-		_strncpy(full_path, dir, dir_len);
-		full_path[dir_len] = '/';
-		_strncpy(full_path + dir_len + 1, command, command_len);
-		full_path[full_path_len] = '\0';
-
-		if (access(full_path, X_OK) == 0)
+	if (command[0] == '.' || command[0] == '/')
+	{
+		if (access(command, X_OK) == 0)
 		{
-			free(path_copy);
+			_strncpy(full_path, command, command_len);
 			return (1);
 		}
-
-		dir = strtok(NULL, ":");
 	}
+	else
+	{
+		dir = strtok(path_copy, ":");
+		while (dir != NULL)
+		{
+			size_t dir_len = _strlen(dir);
+			size_t full_path_len = dir_len + 1 + command_len;
+
+			_strncpy(full_path, dir, dir_len);
+			full_path[dir_len] = '/';
+			_strncpy(full_path + dir_len + 1, command, command_len + 1);
+			full_path[full_path_len] = '\0';
+
+			if (access(full_path, X_OK) == 0)
+			{
+				free(path_copy);
+				return (1);
+			}
+
+			dir = strtok(NULL, ":");
+		}
 	free(path_copy);
+	}
 	return (0);
 }
 
