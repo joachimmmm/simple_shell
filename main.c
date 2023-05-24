90d1e37b1fc547e1a79d9a555fe7d98a0619ac64
achrafelaffas <achrafelaffas@gmail.com>
2023-05-24 05:01:51 -0700

    undo

--- main.c
+++ main.c
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
 		}
 		args[index] = NULL;
 		if (_strcmp(command, "exit") == 0)
-			exit();
+			exit(2);
 		else if (_strcmp(command, "env") == 0)
 			print_env();
 		else if (is_full_path(command))
