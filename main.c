8d28e2b6980d118a7e25653bfd7e215ea626957a
Joachimm <joachimokoh23@gmail.com>
2023-05-25 10:15:36 +0100

    betty fix

--- main.c
+++ main.c
@@ -63,9 +63,7 @@ int main(int argc, char *argv[])
 		}
 		args[index] = NULL;
 		if (_strcmp(command, "exit") == 0)
-		{
-			exit(126);
-		}
+			exit(98);
 		else if (_strcmp(command, "env") == 0)
 			print_env();
 		else if (is_full_path(command))
