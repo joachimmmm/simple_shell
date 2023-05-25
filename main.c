4e1d96814582b0251d79323b1b8f309b288a92ca
Joachimm <joachimokoh23@gmail.com>
2023-05-25 07:56:49 +0100

    changes

--- main.c
+++ main.c
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
 		}
 		args[index] = NULL;
 		if (_strcmp(command, "exit") == 0)
-			exit(1);
+			break;
 		else if (_strcmp(command, "env") == 0)
 			print_env();
 		else if (is_full_path(command))
@@ -71,7 +71,9 @@ int main(int argc, char *argv[])
 		else if (check_command_existence(new_command, full_path))
 			pidf(full_path, args);
 		else
+		{
 			print_error(argv[0], __LINE__, command);
+		}
 	}
 	free(command);
 	return (EXIT_SUCCESS);
