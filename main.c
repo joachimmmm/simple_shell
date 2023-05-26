fc2136cbf300306bb5421c85fd139e89134456d5
Joachimm <joachimokoh23@gmail.com>
2023-05-26 17:19:53 +0100

    exit

--- main.c
+++ main.c
@@ -63,7 +63,7 @@ int main(int argc, char *argv[])
 		}
 		args[index] = NULL;
 		if (_strcmp(command, "exit") == 0)
-			return(2);
+			return(EXIT_SUCCESS);
 		else if (_strcmp(command, "env") == 0)
 			print_env();
 		else if (is_full_path(command))
