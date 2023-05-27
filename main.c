0249458535736800d85f3f049ae3f357bfb8a87d
achrafelaffas <achrafelaffas@gmail.com>
2023-05-27 08:47:07 -0700

    hopeless fix

--- main.c
+++ main.c
@@ -65,7 +65,7 @@ int main(int argc, char *argv[])
 		if (_strcmp(args[0], "exit") == 0)
 		{
 			free(command);
-			exit_program(atoi_func(argv[1]));
+			exit(exit_st(argc));
 		}
 		else if (_strcmp(command, "env") == 0)
 			print_env();
